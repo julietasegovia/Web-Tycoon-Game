591b828e4e5cb4566912136c46ee1caf91a3b91b
Ana <adehezaflaherty@gmail.com>
2026-06-02 20:28:02 -0300

    postgreSQL

--- backend-farm/node_modules/.prisma/client/index-browser.js
+++ backend-farm/node_modules/.prisma/client/index-browser.js
@@ -116,6 +116,9 @@ Prisma.NullTypes = {
  */
 
 exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
+  ReadUncommitted: 'ReadUncommitted',
+  ReadCommitted: 'ReadCommitted',
+  RepeatableRead: 'RepeatableRead',
   Serializable: 'Serializable'
 });
 
@@ -131,6 +134,11 @@ exports.Prisma.UserScalarFieldEnum = {
 exports.Prisma.SortOrder = {
   asc: 'asc',
   desc: 'desc'
+};
+
+exports.Prisma.QueryMode = {
+  default: 'default',
+  insensitive: 'insensitive'
 };
 
 
